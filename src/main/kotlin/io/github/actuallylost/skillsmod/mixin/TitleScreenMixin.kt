4b96429593c7b54b07c6662fdfafefd3544b2333
actuallylost <pandagaminggr@gmail.com>
2024-07-20 02:39:56 +0300

    chore: commented logger

--- src/main/kotlin/io/github/actuallylost/skillsmod/mixin/TitleScreenMixin.kt
+++ src/main/kotlin/io/github/actuallylost/skillsmod/mixin/TitleScreenMixin.kt
@@ -1,6 +1,5 @@
 package io.github.actuallylost.skillsmod.mixin
 
-import io.github.actuallylost.skillsmod.SkillsMod
 import net.minecraft.client.gui.screen.TitleScreen
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
@@ -9,9 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 @Mixin(TitleScreen::class)
 class TitleScreenMixin {
-    
     @Inject(method = ["init"], at = [At("TAIL")])
-    fun onInit(ci: CallbackInfo?) {
-        SkillsMod.LOGGER.info("This line is printed by an example mod mixin!")
+    fun onInit(ci: CallbackInfo) {
+//        SkillsMod.LOGGER.info("This line is printed by an example mod mixin!")
     }
 }
