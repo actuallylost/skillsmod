98d1b1e1bec57cdb30a86af8b5c6f1e6a414da5a
actuallylost <pandagaminggr@gmail.com>
2024-07-20 02:40:38 +0300

    feat: implement attack event handling

--- src/main/kotlin/io/github/actuallylost/skillsmod/events/AttackEntityHandler.kt
+++ src/main/kotlin/io/github/actuallylost/skillsmod/events/AttackEntityHandler.kt
@@ -1,4 +1,32 @@
 ﻿package io.github.actuallylost.skillsmod.events
 
-class AttackEntityHandler {
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback
+import net.minecraft.entity.Entity
+import net.minecraft.entity.passive.AnimalEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.text.Text
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.world.World
+
+class AttackEntityHandler : AttackEntityCallback {
+    override fun interact(
+        player: PlayerEntity?,
+        world: World?,
+        hand: Hand?,
+        entity: Entity?,
+        hitResult: EntityHitResult?
+    ): ActionResult {
+        if (player!!.isSpectator) {
+            return ActionResult.FAIL
+        }
+
+        // TODO: Play around with ServerLivingEntityEvents and see if you can send a message when entity dies, with info about the entity.
+        if (entity is AnimalEntity && !world!!.isClient()) {
+            player.sendMessage(Text.literal(player.name.string + " attacked a " + entity.name.string + "!"), false)
+        }
+
+        return ActionResult.PASS
+    }
 }
