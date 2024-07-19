864789b28516e5310f0de41dfbb7e1e899d44fce
actuallylost <pandagaminggr@gmail.com>
2024-07-20 02:41:07 +0300

    feat: register handler; change from object to class

--- src/main/kotlin/io/github/actuallylost/skillsmod/SkillsMod.kt
+++ src/main/kotlin/io/github/actuallylost/skillsmod/SkillsMod.kt
@@ -1,14 +1,18 @@
 package io.github.actuallylost.skillsmod
 
+import io.github.actuallylost.skillsmod.events.AttackEntityHandler
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object SkillsMod : ModInitializer {
+class SkillsMod : ModInitializer {
     val LOGGER: Logger = LoggerFactory.getLogger("SkillsMod")
 
     override fun onInitialize(mod: ModContainer) {
         LOGGER.info("{} Initialized", mod.metadata().name())
+
+        AttackEntityCallback.EVENT.register(AttackEntityHandler())
     }
 }
