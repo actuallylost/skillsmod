a5cdc739d20669871b285397296feff0afa8d6cc
actuallylost <pandagaminggr@gmail.com>
2024-07-20 01:40:41 +0300

    feat: add Skill class & constructor

--- src/main/kotlin/io/github/actuallylost/skillsmod/SkillType.kt
+++ src/main/kotlin/io/github/actuallylost/skillsmod/SkillType.kt
@@ -1,4 +1,14 @@
 ﻿package io.github.actuallylost.skillsmod
 
-enum class SkillType {
+enum class SkillType(val translationKey: String) {
+    FIGHTING("skill.skillsmod.fighting"),
+    FORAGING("skill.skillsmod.foraging"),
+    MINING("skill.skillsmod.mining"),
+    FARMING("skill.skillsmod.farming"),
+    FISHING("skill.skillsmod.fishing"),
+    ENCHANTING("skill.skillsmod.enchanting"),
+    ALCHEMY("skill.skillsmod.alchemy"),
+    TRADING("skill.skillsmod.trading"),
+    EXPLORING("skill.skillsmod.exploring"),
+    HORSE_RIDING("skill.skillsmod.horse_riding"),
 }
